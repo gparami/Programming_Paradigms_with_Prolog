fb01eef41d4bd6882444dc10b190530ef67da9ae
Parami Gunasekara <parami@live.ca>
2018-01-26 16:04:23 -0500

    q3 completed

--- DGDs/20180125.pl
+++ DGDs/20180125.pl
@@ -7,6 +7,19 @@ clockNextS(c(H,M,S),c(0,0,0))  :-   S =:= 59, M =:= 59, H =:= 23.
 % Q2: How many times is the predicate factorial/2 called with the following query: ?- factorial (3,W).
 factorial (0,1).
 factorial (N,F) :-  N>0,
-                    N1 is N-1,
-                    factorial (N1, F1),
-                    F is N * F1.
+                   N1 is N-1,
+                   factorial (N1, F1),
+                   F is N * F1.
+% W = 6 and predicate will be called 4 times, justify with a tree.
+
+% Q3: Write a program which calculates the Ackermann function
+a(0,N,A) :- A is N+1.
+a(M,0,A) :- M > 0,
+            M1 is M-1,
+            a(M1,1, A).
+a(M,N,A) :- M > 0,
+            N > 0,
+            M1 is M-1,
+            N1 is N-1,
+            a(M,N1,A1),
+            a(M1,A1,A).
