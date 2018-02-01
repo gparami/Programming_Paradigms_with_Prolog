ae0dc1a767a819bee2de371055f78ce83fa815ee
Parami Gunasekara <parami@live.ca>
2018-02-01 10:34:31 -0500

    added the correct looping method.

--- lab03/loop.pl
+++ lab03/loop.pl
@@ -1,2 +1,10 @@
 %Exercise 1 - Looping
-countDown(N):- repeat, writeln(N), N is N-1, N<0, !.
+
+%Looping cannot be done with repeat this way because there is no statement to call N1 as the next countdown command (ie. coundown(N1)).
+%countDown(N):- repeat, writeln(N), N1 is N-1, N1 < 0, !.
+
+%Therefore use recursion
+countDown(N) :- N<0, !.
+countDown(N) :- writeln(N),
+                NN is N-1,
+                countDown(NN).
