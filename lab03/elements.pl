28f63d5aaeb425277435f0a3e8ed714bf4eb5e27
Parami Gunasekara <parami@live.ca>
2018-02-01 10:37:16 -0500

    exercise2

---
+++ lab03/elements.pl
@@ -0,0 +1,11 @@
+%Exercise 2. Chemical Elements
+
+%Given the following database of chemical elements and their symbols in the periodic table.
+element(chlorine,'Cl').
+element(helium,'He').
+element(hydrogen,'H').
+element(nitrogen,'N').
+element(oxygen,'O').
+
+%Write an interactive Prolog program for the user to enter a symbol and the program prints the name of the corresponding chemical element.
+%If the symbol is not in the database, the program should exit otherwise loop again.
