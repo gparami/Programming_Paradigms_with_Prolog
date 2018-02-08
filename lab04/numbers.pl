a4335ac2297eb5ff53807011dad63f1ee84bb72d
Parami Gunasekara <parami@live.ca>
2018-02-08 10:37:07 -0500

    Exercise 4

---
+++ lab04/numbers.pl
@@ -0,0 +1,13 @@
+% Write a predicate to add up all the numbers in a list but uses an alternating sign.
+
+addAlternate(L,S) :- addAlternate(L,0,_,S].
+
+addAlternate([],S,_,S).
+
+addAlternate([H|T],A,p,S) :- !,
+	AA is A + H,
+	addAlternate(T,AA,m,S).
+
+addAlternate([H|T],A,m,S) :- !, 
+	AA is A - H,
+        addAlternate(T,AA,p,S).
