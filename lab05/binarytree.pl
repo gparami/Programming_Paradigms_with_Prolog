9473820c1dce7d96c4e6faafc0604377804af175
Parami Gunasekara <parami@live.ca>
2018-02-15 11:22:23 -0500

    ex4 not working

--- lab05/binarytree.pl
+++ lab05/binarytree.pl
@@ -1,15 +1,16 @@
 % Write a program that finds all nodes in a binary tree that have a left subtree but no right subtree (nil).
 % The program is to return these nodes in a inorder list.
 
+treeA(X) :- X = t(73, t(31, t(5, nil, nil), nil), t(101, t(83, t(97, nil, nil), nil), nil)).
+
 leftNodes(T,L) :- leftNodes(T,[],L).
 
 leftNodes(nil,LA,LA) :- !.
 
 leftNodes(t(Root,Left,nil),LA,L) :- 
-    ______________________,
-    ______________________,
+    
     leftNodes(Left,LA,LL),
-    ______________________.
+    append(Root,LA,L).
 
 leftNodes(t(_,Left,Right),LA,L) :-
     leftNodes(Left,LA,LL),
