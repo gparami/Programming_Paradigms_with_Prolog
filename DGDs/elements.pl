a307d83dd9098946777128faafc8b6fe1db6970e
Parami Gunasekara <parami@live.ca>
2018-02-28 11:47:18 -0500

    recursion tutorial

--- DGDs/elements.pl
+++ DGDs/elements.pl
@@ -5,7 +5,13 @@ lastElement([], X, X).
 lastElement([H|T], X) :- lastElement(T, H, X).
 lastElement([H|T], _, X) :- lastElement(T, H, X).
 
-countElements([], X, X).
-countElements([H|T], X) :-  countElements( T, 1, X).
-countElements([H|T], X1, X) :-  countElements(T, X1, X),
-                                X is X1 + 1.
+% countElements([], 0).
+% countElements([E], _, 1).
+% countElements(L, X) :-  countElements(L, X, 0).
+% countElements([_|T], X, C) = C1 is C + 1, countElements(T, X, C1), X is C1.
+
+
+countElements([], 0).
+countElements([_|T], X) :-  countElements(T, X1), X is X1 + 1.
+
+listInsert(X,L,R) :- R = [X|L].
